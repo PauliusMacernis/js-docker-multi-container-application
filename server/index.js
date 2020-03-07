36d9160299d89cd16e89dca9dc2803a269ffc93f
Paulius Macernis <paulius@emotion.lt>
2020-03-07 14:22:33 +0200

    115. More Express API Setup, project diagram added.

--- server/index.js
+++ server/index.js
@@ -21,6 +21,52 @@ const pgClient = new Pool({
 
 pgClient.on('error', () => console.log('Lost Postgres connection'));
 
-// Create a table to store count values if the table does not exist yet.
+// - Create a table to store count values if the table does not exist yet.
 pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((err) => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+   host: keys.redisHost,
+   port: keys.redisPort,
+   retry_strategy: () => 1000 // 1 second
+});
+const redisPublisher = redisClient.duplicate(); // See redis docs for more info on why to duplicate
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    // ...we use this callback because redis library for node.js does not support promises
+    redisClient.hgetall('values', (err, values) => {
+       res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true });
+});
+
+app.listen(5000, err => {
+   console.log('Listening');
+});
